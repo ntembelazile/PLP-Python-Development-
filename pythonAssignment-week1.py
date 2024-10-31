c9b85cf67e31c000162dccc1f006ff0b89992505
ntembelazile <64739543+ntembelazile@users.noreply.github.com>
2024-11-01 01:02:12 +0200

    Add files via upload

---
+++ pythonAssignment-week1.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+age = input("What is your age? ")
+location = input("Where do you stay? ")
+
+print(f"Hello, {name}, you are {age} years old and you live in {location}")
